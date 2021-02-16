9aec5c4ed6586d7fdb9bfc41173c3e2d191b3365
JesseGit5151 <jrodriguez5151yahoo.com>
2021-02-15 20:51:13 -0800

    finished

---
+++ JS/script.js
@@ -0,0 +1,114 @@
+//Step 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction
+
+//Step 2
+// 12
+// "string"
+// true
+// null
+
+//Step 3
+// let problem = 3 * numTotal;
+// let fullName = firstName + lastName;
+
+//Step 4
+// let firstName; 
+// let lastName; 
+// let address;
+// let city;
+// let state; 
+// let zipCode; 
+// let yourAge; 
+// let referralSource; 
+// let mayWeContactYou;
+
+//Step 5
+// let firstName;
+// firstName = "Jesse";
+// let lastName = "Rodriguez";
+// let city = "San Diego", lastName = "Rodriguez";
+
+//Step 6
+// let num = 12 + "34";
+// console.log(num)
+
+//Step 7
+// let foo = true + "string";
+// console.log(foo);
+// let num = false + 12;
+// console.log(num)
+
+//Step 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+//Step 9
+// let score = null;
+// console.log(score);
+// let un;
+// console.log(un)
+
+//Step 10
+// console.log(typeof("yesterday"));
+// console.log(typeof(12));
+// console.log(typeof(true));
+// console.log(typeof([]));
+// console.log(typeof(un))
+
+//Step 11
+// alert('Hello ' + "Jesse Rodriguez" + ', welcome to the JavaScript class!');
+
+//Step 12
+// let fullName = "Jesse Rodriguez";
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!');
+
+//Step 13
+// let course = "Javascript";
+// alert('Hello ' + "Jesse Rodriguez" + ', welcome to the ' + course + ' class!');
+
+//Step 14
+// let course = "Javascript";
+// alert('Hello ' + "Jesse Rodriguez" + '.' + "\n" + 'Welcome to the ' + course + ' class!');
+
+//Step 15
+// let namePrompt = prompt("Please enter your name.");
+// alert('Hello ' + namePrompt + ', welcome to the JavaScript class!');
+
+//Step 16
+// let course = prompt("Enter your class name.");
+// alert('Hello ' + "Jesse Rodriguez" + ', welcome to the ' + course + ' class!');
+
+//Step 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+//Step 18
+// let x = 20;
+// console.log(x + 20);
+
+//Step 19
+// let x = 20;
+// console.log(x * 5);
+
+//Step 20
+// let x = 20 % 3;
+// console.log(x / 1);
+
+//Step 21
+// let x = 8;
+// let y = 3;
+// if(x === 8 && y === 3) {
+//     console.log(x)
+// }
+
+//Step 22
+// let x = 9;
+// let y = 5;
+// if(x != 1 || y === 10) {
+//     console.log(false)
+// }
